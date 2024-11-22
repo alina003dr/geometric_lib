beef0754f78c40029f9287cfb5890db873821fce
Alina <alina003dr@mail.ru>
2024-11-22 20:36:17 +0300

    Tests

--- calculate.py
+++ calculate.py
@@ -1,17 +1,44 @@
 import circle
 import square
+import triangle
 
-
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+    'circle-area': 1,
+    'circle-perimeter': 1,
+    'square-area': 1,
+    'square-perimeter': 1,
+    'triangle-area': 3,
+    'triangle-perimeter': 3
+}
+
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+    # Проверка наличия передаваемого аргумента fig in figs
+    assert fig in figs
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+    # Проверка наличия передаваемого аргумента func in funcs
+    assert func in funcs
+
+    # Проверка на соответсвие количества аргументов
+    key = f'{fig}-{func}'
+    expected_args = sizes.get(key)
+    assert expected_args is not None
+    assert len(size) == expected_args
+
+    # Проверка аргумента на неотрицание
+    assert all(s >= 0 for s in size)
+
+    # Проверка существование тругольника
+    if fig == 'triangle':
+        a, b, c = size
+        assert a + b > c and a + c > b and c + b > a
+
+    # Вызов функции
+    module = globals()[fig]
+    result = getattr(module, func)(*size)
+    return result
 
 if __name__ == "__main__":
 	func = ''
@@ -28,6 +55,3 @@ if __name__ == "__main__":
 		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
 	
 	calc(fig, func, size)
-
-
-
