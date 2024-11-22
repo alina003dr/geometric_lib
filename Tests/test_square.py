97ca2b93bb0857d4374dfe9068af8993100b8ed0
Alina <alina003dr@mail.ru>
2024-11-22 23:38:36 +0300

    try10

--- Tests/test_square.py
+++ Tests/test_square.py
@@ -3,33 +3,24 @@ from square import area, perimeter
 
 
 class TestSquare(unittest.TestCase):
-
     def test_area(self):
         side = 1
-        
         result = area(side)
-        
         self.assertEqual(result, 1)
 
     def test_area_zero(self):
         side = 0
-        
         result = area(side)
-        
         self.assertEqual(result, 0)
 
     def test_perimeter(self):
         side = 1
-        
         result = perimeter(side)
-        
         self.assertEqual(result, 4)
 
     def test_perimeter_zero(self):
         side = 0
-        
         result = perimeter(side)
-        
         self.assertEqual(result, 0)
 
 
--- Tests/test_triangle.py
+++ Tests/test_triangle.py
@@ -5,16 +5,12 @@ from triangle import perimeter, area
 class TestTriangle(unittest.TestCase):
     def test_area(self):
         a, b, c = 3, 4, 5
-        
         result = area(a, b, c)
-        
         self.assertEqual(result, 6)
 
     def test_perimeter(self):
         a, b, c = 2, 3, 4
-        
         result = perimeter(a, b, c)
-        
         self.assertEqual(result, 9)
 
 
