6cc9d7c601d31f8b1afe50b927592facf3d8ae8b
Alina <139043417+alina003dr@users.noreply.github.com>
2024-11-23 20:27:41 +0300

    Update test_calculate.py

--- Tests/test_calculate.py
+++ Tests/test_calculate.py
@@ -66,6 +66,55 @@ class TestCalculate(unittest.TestCase):
         with self.assertRaises(AssertionError):
             calc(fig, func, size)
 
+    def test_neg_size_area_circle(self):
+        fig = 'circle'
+        func = 'area'
+        size = [-1]
+        with self.assertRaises(AssertionError):
+            calc(fig, func, size)
+
+    def test_neg_size_area_square(self):
+        fig = 'square'
+        func = 'area'
+        size = [-1]
+        with self.assertRaises(AssertionError):
+            calc(fig, func, size)
+
+    def test_neg_size_area_triangle(self):
+        fig = 'triangle'
+        func = 'area'
+        size = [-5, -12, -13]
+        with self.assertRaises(AssertionError):
+            calc(fig, func, size)
+
+    def test_neg_size_perimeter_circle(self):
+        fig = 'circle'
+        func = 'perimeter'
+        size = [-1]
+        with self.assertRaises(AssertionError):
+            calc(fig, func, size)
+
+    def test_neg_size_perimeter_square(self):
+        fig = 'square'
+        func = 'perimeter'
+        size = [-1]
+        with self.assertRaises(AssertionError):
+            calc(fig, func, size)
+
+    def test_neg_size_perimeter_triangle(self):
+        fig = 'triangle'
+        func = 'perimeter'
+        size = [-5, -12, -13]
+        with self.assertRaises(AssertionError):
+            calc(fig, func, size)
+
+    def test_wrong_size_triangle(self):
+        fig = 'triangle'
+        func = 'area'
+        size = [1, 2, 10]
+        with self.assertRaises(AssertionError):
+            calc(fig, func, size)
+
 
 if __name__ == '__main__':
     unittest.main()
